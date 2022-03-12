ce940d70c33762d11b9ccb12fd0ce693b32aebec
Yash Gupta <yash3144@gmail.com>
2022-03-12 13:08:13 +0530

    use currentHeight Property of StatusBar

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
 
+// const isAndroid = Platform.OS==='android'
 export default function App() {
+  console.log(StatusBar.currentHeight)
   return (
-    <View style={{flex:1,alignItems:"center", justifyContent:"center"}}>
-      <Text>Our blank react native  application</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight}}>
+        <View
+          style={{
+            padding: 16,
+            justifyContent: "center",
+            backgroundColor: "green",
+          }}
+        >
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto"/>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
