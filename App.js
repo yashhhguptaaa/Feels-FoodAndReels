38dd0fd723628bc0e948f3e04b7daf3a09b6d18a
Yash Gupta <yash3144@gmail.com>
2022-03-14 00:22:31 +0530

    Add different themes in files and wrap app.js with themeProvider

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { theme } from "./src/infrastructure/theme";
+
+import { ThemeProvider } from "styled-components/native";
 
 // const isAndroid = Platform.OS==='android'
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
