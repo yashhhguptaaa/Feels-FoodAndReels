06f0a45175e9f1f8a02f3fd82a78d67d1eb61604
Yash Gupta <yash3144@gmail.com>
2022-03-14 00:48:34 +0530

    install expo-google-fonts package and use fonts in components

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
 
@@ -8,6 +14,18 @@ import { ThemeProvider } from "styled-components/native";
 
 // const isAndroid = Platform.OS==='android'
 export default function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
