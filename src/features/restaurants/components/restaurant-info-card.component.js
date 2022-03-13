6b4de02c4a87e61ab6838810cc3a15185610914c
Yash Gupta <yash3144@gmail.com>
2022-03-14 00:23:13 +0530

    User color theme in title component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -13,6 +13,7 @@ const RestaurantCardCover = styled(Card.Cover)`
 
 const Title = styled.Text`
   padding: 16px;
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
