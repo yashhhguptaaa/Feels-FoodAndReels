8074052187c8589d736ed913436b032d4b0cb515
Yash Gupta <yash3144@gmail.com>
2022-03-14 12:53:36 +0530

    use props theme in order to give spacing

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -3,51 +3,51 @@ import { View } from "react-native";
 import styled from "styled-components/native";
 
 const TopSmall = styled.View`
-  margin-top: 4px;
+  margin-top: ${(props) => props.theme.space[1]};
 `;
 
 const TopMedium = styled.View`
-  margin-top: 8px;
+  margin-top: ${(props) => props.theme.space[2]};
 `;
 
 const TopLarge = styled.View`
-  margin-top: 16px;
+  margin-top: ${(props) => props.theme.space[3]};
 `;
 
 const LeftSmall = styled.View`
-  margin-left: 4px;
+  margin-left: ${(props) => props.theme.space[1]};
 `;
 
 const LeftMedium = styled.View`
-  margin-left: 8px;
+  margin-left: ${(props) => props.theme.space[2]};
 `;
 
 const LeftLarge = styled.View`
-  margin-left: 16px;
+  margin-left: ${(props) => props.theme.space[3]};
 `;
 
 const RightSmall = styled.View`
-  margin-right: 4px;
+  margin-right: ${(props) => props.theme.space[1]};
 `;
 
 const RightMedium = styled.View`
-  margin-right: 8px;
+  margin-right: ${(props) => props.theme.space[2]};
 `;
 
 const RightLarge = styled.View`
-  margin-right: 16px;
+  margin-right: ${(props) => props.theme.space[3]};
 `;
 
 const BottomSmall = styled.View`
-  margin-bottom: 4px;
+  margin-bottom: ${(props) => props.theme.space[1]};
 `;
 
 const BottomMedium = styled.View`
-  margin-bottom: 8px;
+  margin-bottom: ${(props) => props.theme.space[2]};
 `;
 
 const BottomLarge = styled.View`
-  margin-bottom: 16px;
+  margin-bottom: ${(props) => props.theme.space[3]};
 `;
 
 export const Spacer = ({ variant }) => {
