e819ee2a214da2595d299491850b85f9da9261f0
Yash Gupta <yash3144@gmail.com>
2022-03-28 13:31:32 +0530

    Fix: Add photos from mockImages

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,4 @@
-import { mocks } from "./mock";
+import { mocks, mockImages } from "./mock";
 import camelize from "camelize";
 
 export const restaurantsRequest = (location = "41.878113,-87.629799") => {
@@ -13,7 +13,9 @@ export const restaurantsRequest = (location = "41.878113,-87.629799") => {
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
-    restaurant.photos = [];
+    restaurant.photos = restaurant.photos.map((p) => {
+      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+    });
     return {
       ...restaurant,
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
