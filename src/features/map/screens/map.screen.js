871e4202f5a87d45e379412971da40f77b594204
Yash Gupta <yash3144@gmail.com>
2022-05-08 14:25:51 +0530

    Define custom search component for the map View

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -2,9 +2,16 @@ import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components";
 
+import { Search } from "../components/search.component";
+
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
-export const MapScreen = () => <Map />;
+export const MapScreen = () => (
+  <>
+    <Search />
+    <Map />
+  </>
+);
