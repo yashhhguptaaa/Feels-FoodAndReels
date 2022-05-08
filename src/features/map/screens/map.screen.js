318ac3555e6b36c95e82d0c12acd9d91c6f9ebac
Yash Gupta <yash3144@gmail.com>
2022-05-08 14:12:58 +0530

    Render map with styled component

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,4 +1,10 @@
 import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
+import styled from "styled-components";
 
-export const MapScreen = () => <MapView style={{ height: "100%" }} />;
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
+
+export const MapScreen = () => <Map />;
