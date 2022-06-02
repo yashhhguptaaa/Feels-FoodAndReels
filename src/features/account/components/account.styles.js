3df31fe7b7040c079de7aef3343167ccf6fcdbfa
Yash Gupta <yash3144@gmail.com>
2022-06-03 02:08:28 +0530

    Apply background image in screens

---
+++ src/features/account/components/account.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const AccountBackground = styled.ImageBackground.attrs({
+  source: require("../../../../assets/home_bg.jpg"),
+})`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
