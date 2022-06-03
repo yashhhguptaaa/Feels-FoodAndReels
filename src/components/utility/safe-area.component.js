cc22941993d4cf8df148e0cd2816d33529f70112
Yash Gupta <yash3144@gmail.com>
2022-06-03 23:52:04 +0530

    Set background to pure white

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -3,4 +3,5 @@ import styled from "styled-components/native";
 export const SafeArea = styled(SafeAreaView)`
   flex: 1px;
   ${StatusBar.currentHeight && `margin-top : ${StatusBar.currentHeight}px`};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
