d2ebde02f94cd3aa719c192bbd32a5e19ad79c6a
Yash Gupta <yash3144@gmail.com>
2022-06-03 18:45:55 +0530

    Create Route for Login and Register Button

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { colors } from "../../../infrastructure/theme/colors";
+import { Button } from "react-native-paper";
 
 export const AccountBackground = styled.ImageBackground.attrs({
   source: require("../../../../assets/home_bg.jpg"),
@@ -13,4 +15,16 @@ export const AccountCover = styled.View`
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.2);
+`;
+
+export const AccountContainer = styled.View`
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: ${(props) => props.theme.space[4]};
+  margin-top: ${(props) => props.theme.space[2]};
+`;
+
+export const AuthButton = styled(Button).attrs({
+  color: colors.brand.primary,
+})`
+  padding: ${(props) => props.theme.space[2]};
 `;
